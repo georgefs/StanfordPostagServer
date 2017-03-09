08a7e3e1262313a21aefa67e6ef9e4ef5cd71d20
george <goblin_george@gmail.com>
2017-03-09 23:29:14 +0800

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+wget http://nlp.stanford.edu/software/stanford-segmenter-2015-12-09.zip
+unzip stanford-segmenter-2015-12-09.zip
+rm -f stanford-segmenter-2015-12-09.zip
+cp -r stanford-segmenter-2015-12-09/*.jar lib
+rm -rf stanford-segmenter-2015-12-09/
+
 wget http://nlp.stanford.edu/software/stanford-postagger-full-2015-12-09.zip
 unzip stanford-postagger-full-2015-12-09.zip
 rm -f stanford-postagger-full-2015-12-09.zip
 cp -r stanford-postagger-full-2015-12-09/*.jar lib
 rm -rf stanford-postagger-full-2015-12-09
-
 
 sh make.sh
--- src/SegServicePublisher.java
+++ src/SegServicePublisher.java
@@ -1,14 +1,12 @@
 package service.endp;
-import service.impl.PosServiceImpl;
+import service.impl.SegServiceImpl;
 
 import javax.xml.ws.Endpoint;
 
 //Endpoint publisher
-@SpringBootApplication
-public class PosServicePublisher{
+public class SegServicePublisher{
 
 	public static void main(String[] args) {
-		Endpoint.publish("http://0.0.0.0:9998/pos", new PosServiceImpl());
 		Endpoint.publish("http://0.0.0.0:9999/seg", new SegServiceImpl());
     }
 
