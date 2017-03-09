20f4fc298f45c4f853e539d024deefb7bc0ae1eb
george <goblin_george@gmail.com>
2017-03-09 23:05:00 +0800

    update

--- install.sh
+++ install.sh
@@ -3,14 +3,8 @@
 wget http://nlp.stanford.edu/software/stanford-postagger-full-2015-12-09.zip
 unzip stanford-postagger-full-2015-12-09.zip
 rm -f stanford-postagger-full-2015-12-09.zip
-cp -r stanford-postagger-full-2015-12-09/* .
+cp -r stanford-postagger-full-2015-12-09/*.jar lib
 rm -rf stanford-postagger-full-2015-12-09
 
-unzip -o stanford-postagger.jar
-unzip -o slf4j-api.jar
-unzip -o slf4j-simple.jar
-
-mkdir -p service/impl
-mkdir -p service/endp
 
 sh make.sh
--- make.sh
+++ make.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-javac -cp . PosService.java
-mv -f PosService.class service/impl/
-
-javac -cp . PosServiceImpl.java
-mv -f PosServiceImpl.class service/impl/
-
-javac PosServicePublisher.java
-mv -f PosServicePublisher.class service/endp/
+javac -cp lib/*:. -d . src/*.java
